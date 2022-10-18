9a23c8b0ea2104ae1eff757ce372a646afdc9970
Anton Popov <anton@bbbro.ru>
2022-10-18 03:55:09 +0300

    feat: добавить компонент RegularText

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import MainTitle from './components/MainTitle/MainTitle';
 import RegularTitle from './components/RegularTitle/RegularTitle';
+import RegularText from './components/RegularText/RegularText';
 
 function App() {
   return (
     <>
-      <MainTitle text='Любимовка. Ещё'/>
-      <RegularTitle text='О проекте'/>
+      <MainTitle text='Любимовка. Ещё' />
+      <RegularTitle text='О проекте' />
+      <RegularText
+        text='За первый сезон проекта организаторы провели 17 мероприятий вместе с режиссёрами и актёрами московских театров: Театра.doc, Театрального Центра им. Вс. Мейерхольда, Гоголь-центра, Электротеатра «Станиславский», Ленкома.'
+      />
     </>
   );
 }
