54aa2ca6f35b16b90a0089f4b0a0999b5a3c245f
Veronika Paist <nika.kuleva@yandex.ru>
2022-10-18 18:05:58 +0300

    feat/smallButton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SmallButton from './components/SmallButton/SmallButton';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SmallButton text='ПРОЕКТЫ'/>
     </div>
   );
 }
