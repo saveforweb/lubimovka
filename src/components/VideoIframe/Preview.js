f6bcf39c02c7b876bbad322d4a444f3d9785c538
Anton Popov <anton@bbbro.ru>
2022-10-20 00:35:46 +0300

    fix: поправить ховер иконки видео

--- src/components/VideoIframe/Preview.js
+++ src/components/VideoIframe/Preview.js
@@ -4,13 +4,25 @@ function Preview(props) {
 
     return (
         <>
-            <a className="video-iframe__link" target="_blank" onClick={onCLick}>
+            <div className="video-iframe__block" onClick={onCLick}>
                 <picture>
                     <source srcSet={`https://i.ytimg.com/vi_webp/${id}/maxresdefault.webp`} type="image/webp" />
                     <img className="video-iframe__media" src={`https://i.ytimg.com/vi/${id}/maxresdefault.jpg`} alt="" />
                 </picture>
-            </a>
-            <button className="video-iframe__button" aria-label='Запустить видео' onClick={onCLick}></button>
+                <button className="video-iframe__button" aria-label='Запустить видео'>
+                    <svg
+                        viewBox="0 0 64 64"
+                        fill="none" xmlns="http://www.w3.org/2000/svg"
+                        className="video-iframe__icon">
+                        <rect
+                            rx="32"
+                            className="video-iframe__icon-circle" />
+                        <path
+                            d="M41 31.4999L26.75 39.7272L26.75 31.4999L26.75 23.2727L41 31.4999Z"
+                            className="video-iframe__icon-triangle" />
+                    </svg>
+                </button>
+            </div>
         </>
     )
 }
