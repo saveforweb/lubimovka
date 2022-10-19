2de33b642e57e48827adebc95274c02e9956c1ff
Veronika Paist <nika.kuleva@yandex.ru>
2022-10-19 17:14:02 +0300

    Feat: HeaderText

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SmallLogo from './components/SmallLogo/SmallLogo';
 import About from './components/About/About';
 import VideoIframe from './components/VideoIframe/VideoIframe';
 import SmallButton from './components/SmallButton/SmallButton';
+import HeaderText from './components/HeaderText/HeaderText';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <SmallLogo />
       <SmallButton text='ПРОЕКТЫ'/>
       <MainTitle text='Любимовка. Ещё' />
+      <HeaderText text='Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки.' />
       <About />
       <VideoIframe id="4JS70KB9GS0"/>
     </>
