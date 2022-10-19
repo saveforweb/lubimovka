15456c615f150e2404edea5549d33318ff511097
Aleksandr Sadykov <Aleksandr2697@yandex.ru>
2022-10-20 01:52:46 +0300

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,10 @@
 export default function Header(props) {
     const { children } = props;
     return (
-        <div>{children}</div>
+        <div>
+            {children}
+            <a href="#"></a>
+            </div>
 
 
 
