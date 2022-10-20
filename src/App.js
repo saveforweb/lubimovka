c407bf59915993482ec36bfe0b83b64e7e4131f9
Anton Popov <anton@bbbro.ru>
2022-10-20 13:23:23 +0300

    fix: исправление ошибок в develop ветке

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './components/About/About';
 import SmallButton from './components/SmallButton/SmallButton';
 import HeaderText from './components/HeaderText/HeaderText';
 import HeaderPhoto from './components/HeaderPhoto/HeaderPhoto';
+import Header from './components/Header/Header';
+import VideoIframe from './components/VideoIframe/VideoIframe';
 
 function App() {
   return (
